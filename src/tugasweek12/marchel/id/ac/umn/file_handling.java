23d3dc92fac3877db4e319aee9c9aecaa7cc67a0
marchelraikonnen <marchelraikonnen19@gmail.com>
2025-11-26 10:37:57 +0700

    Create A File

---
+++ src/tugasweek12/marchel/id/ac/umn/file_handling.java
@@ -0,0 +1,22 @@
+package tugasweek12.marchel.id.ac.umn;
+
+import java.io.File;
+import java.io.IOException;
+
+public class file_handling {
+
+    public static void main(String[] args) {
+        try {
+            File Obj = new File("myfile.txt");
+            if (Obj.createNewFile()) {
+                System.out.println("File created: " + Obj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+
