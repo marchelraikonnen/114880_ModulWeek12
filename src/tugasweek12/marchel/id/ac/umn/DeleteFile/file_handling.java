3f1de886f5bf1b75950f34deee75fe76a4b62a3e
marchelraikonnen <marchelraikonnen19@gmail.com>
2025-11-26 10:58:09 +0700

    Delete myfile.txt

---
+++ src/tugasweek12/marchel/id/ac/umn/DeleteFile/file_handling.java
@@ -0,0 +1,16 @@
+package tugasweek12.marchel.id.ac.umn.DeleteFile;
+
+import java.io.File;
+
+public class file_handling {
+
+    public static void main(String[] args) {
+
+        File Obj = new File("myfile.txt");
+        if (Obj.delete()) {
+            System.out.println("The deleted file is : " + Obj.getName());
+        } else {
+            System.out.println("Failed in deleting the file.");
+        }
+    }
+}
