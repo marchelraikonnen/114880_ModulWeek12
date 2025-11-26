eba22846e781e9fb798cc271c175f2cb41878a56
marchelraikonnen <marchelraikonnen19@gmail.com>
2025-11-26 10:46:00 +0700

    Read myfile.txt

---
+++ src/tugasweek12/marchel/id/ac/umn/CreateFile/file_handling.java
@@ -0,0 +1,26 @@
+package tugasweek12.marchel.id.ac.umn.CreateFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class file_handling {
+
+    public static void main(String[] args) {
+
+        try {
+            File Obj = new File("myfile.txt");
+            Scanner Reader = new Scanner(Obj);
+            while (Reader.hasNextLine()) {
+                String data = Reader.nextLine();
+                System.out.println(data);
+            }
+            Reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+
+
